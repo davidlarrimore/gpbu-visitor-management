e707a49584013bee8f312d77bb299ac0bbb39bf2
David Larrimore <davidlarrimore@gmail.com>
2020-07-23 10:27:54 -0400

    Initial Commit

---
+++ scripts/ConvertProject.sh
@@ -0,0 +1,21 @@
+echo "*** Creating PKG Directory ..."
+mkdir mdapipkg
+
+echo "*** pulling change set ..."
+sfdx force:mdapi:retrieve -s -r ./mdapipkg -p "visitor_management" -u CrisisManagementProd -w 10
+
+echo "*** Cleaning up ..."
+cd mdapipkg
+unzip unpackaged.zip -d
+rm unpackaged.zip
+cd ../
+
+echo "*** Converting to source ..."
+sfdx force:mdapi:convert -r ./mdapipkg/
+
+echo "*** Removing pkg directory ..."
+rm -r ./mdapipkg
+
+
+echo "*** Pushing metadata to scratch org ..."
+sfdx force:source:push
