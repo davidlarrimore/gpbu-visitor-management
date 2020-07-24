886fce6a03beb291f86cf9743ab0cc52e6ff8664
David Larrimore <davidlarrimore@gmail.com>
2020-07-24 12:32:44 -0400

    Trying to stabilize

--- scripts/CreateScratchOrg.sh
+++ scripts/CreateScratchOrg.sh
@@ -1,7 +1,7 @@
 echo "*** Creating scratch org ..."
 sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias MyComponentsScratch -d 30
 
-echo "*** Opening scratch org ..."
+#echo "*** Opening scratch org ..."
 #sfdx force:org:open
 
 echo "*** Installing Dependent Packages ..."
@@ -12,11 +12,8 @@ sfdx force:package:install --package 04t1K000002J0KK -w 1000 -u MyComponentsScra
 echo "*** Pushing metadata to scratch org ..."
 sfdx force:source:push
 
-echo "*** Assigning permission set to your user ..."
-sfdx force:user:permset:assign --permsetname gPBU_Package_Manager
-
 echo "*** Generating password for your user ..."
 sfdx force:user:password:generate --targetusername MyComponentsScratch
 
-echo "*** Creating data"
-sfdx force:data:tree:import -f ./data/export-demo-GPBU_Package__cs.json -u MyComponentsScratch
+#echo "*** Creating data"
+#sfdx force:data:tree:import -f ./data/export-demo-GPBU_Package__cs.json -u MyComponentsScratch
