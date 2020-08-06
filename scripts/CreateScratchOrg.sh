8e3e8d9fff327b1d78f8fc1b3237e5afee562497
David Larrimore <davidlarrimore@gmail.com>
2020-08-06 13:50:19 -0400

    Updated with Data

--- scripts/CreateScratchOrg.sh
+++ scripts/CreateScratchOrg.sh
@@ -5,8 +5,13 @@ sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --
 #sfdx force:org:open
 
 echo "*** Installing Dependent Packages ..."
+#GPBU Package Manager
+#sfdx force:package:install --package 0Ho3h000000k9sWCAQ -w 1000 -u MyComponentsScratch 
+
+#Unnoficial SF
 sfdx force:package:install --package 04t1t0000034vZjAAI -w 1000 -u MyComponentsScratch
 sfdx force:package:install --package 04t1K000002J0KK -w 1000 -u MyComponentsScratch
+
 #sfdx force:package:install --package 04t6C000000HrR5 -w 1000 -u MyComponentsScratch
 
 echo "*** Pushing metadata to scratch org ..."
@@ -15,6 +20,6 @@ sfdx force:source:push
 echo "*** Generating password for your user ..."
 sfdx force:user:password:generate --targetusername MyComponentsScratch
 
-echo "*** Creating data"
+echo "*** Pushing data"
 #sfdx force:data:tree:import -f ./data/Facility__cs.json,./data/Accounts.json -u MyComponentsScratch
 sfdx sfdmu:run --sourceusername csvfile --targetusername MyComponentsScratch --path ./data/
--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -1,24 +1,30 @@
-#No Longer Needed
-#echo "*** Get Source Change Set ..."
-#sfdx force:mdapi:retrieve -r ./mdapipkg -u IDO -p gpbu_demo_tools
-#unzip ./mdapipkg/unpackaged.zip
-#sfdx force mdapi:convert -r mdapipkg/
-
-#No Longer Needed
-#echo "*** import change set ..."
-#sfdx force:mdapi:retrieve -r ./mdapipkg -u IDO -p gpbu_demo_tools
-
 
 #No Longer Needed
 #echo "*** Creating Managed Package ..."
 #sfdx force:package:create --name "Demo Reset Tools" --packagetype Managed --path "force-app"
 #sfdx force:package1:version:create --packageid 033xx00000007oi --name ”Spring 17” --description ”Spring 17 Release” --version 3.2 --managedreleased
 
+echo "*** Updating Org Data ..."
+sfdx force:apex:execute -f ./scripts/apex/updateData.apex 
+
+echo "*** Pulling Data from Org ..."
+#sfdx force:data:tree:import -f ./data/Facility__cs.json,./data/Accounts.json -u MyComponentsScratch
+sfdx sfdmu:run --sourceusername MyComponentsScratch --targetusername csvfile --path ./data/
+
+echo "*** Creating JSON Files ..."
+csvtojson ./data/Account.csv > ./data/json/Account.json
+csvtojson ./data/Contact.csv > ./data/json/Contact.json
+csvtojson ./data/RecordType.csv > ./data/json/RecordType.json
+csvtojson ./data/Room__c.csv > ./data/json/Room__c.json
+csvtojson ./data/Facility_c.csv > ./data/json/Facility_c.json
+csvtojson ./data/Visit_Confirmation__c.csv > ./data/json/Visit_Confirmation__c.json
+csvtojson ./data/UserAndGroup.csv > ./data/json/UserAndGroup.json
+
 #echo "*** Creating Unlocked Package ..."
 #sfdx force:package:create --name "gPBU Component Package Manager Unlocked" --packagetype Unlocked --path "force-app"
 
-#echo "*** Creating Package Version..."
-#sfdx force:package:version:create --package "Demo Reset Tools" -x --wait 10 --codecoverage
+
+
 
 echo "*** Creating Unlocked Package Version..."
 sfdx force:package:version:create --package "gPBU Component Package Manager" -x --wait 10 --codecoverage 
